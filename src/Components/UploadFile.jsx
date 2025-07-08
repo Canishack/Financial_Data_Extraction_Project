c0988f1d3ef7ee2328d329a45ffff4ccb179489e
Kanishak Singh <kanishak1610@gmail.com>
2025-07-08 06:54:54 +0530

    Initial Commit-Project Created

---
+++ src/Components/UploadFile.jsx
@@ -0,0 +1,16 @@
+//function of this component
+//1 Upload a File(excel,pdf,image)
+//2 Store file in React state
+//3 Send it to backend server using axios
+//4 Show success or error messages
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+
+const UploadFile = () => {
+  return (
+    <div>UploadFile</div>
+  )
+}
+
+export default UploadFile
